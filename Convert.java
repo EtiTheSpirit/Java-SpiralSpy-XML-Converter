85aee3eb0c1adae1b374228ec12462fd9b58e435
Brent Duanne <subzeroxan@gmail.com>
2015-01-19 08:54:44 -0600

    Now uses OBJExporter.java for conversion

--- Convert.java
+++ Convert.java
@@ -25,34 +25,7 @@ public class Convert {
 	
 	static int end = 0;
 	
-	private static void IterateTriangleDataTable(double[] stuph) {
-		int len = stuph.length;
-		for (int i = 0; i < len; i++) {
-			if (i == 0) {
-				x1 = stuph[i];
-			} else if (i == 1) {
-				y1 = stuph[i];
-			} else if (i == 2) {
-				z1 = stuph[i];
-			} else if (i == 3) {
-				x2 = stuph[i];
-			} else if (i == 4) {
-				y2 = stuph[i];
-			} else if (i == 5) {
-				z2 = stuph[i];
-			} else if (i == 6) {
-				x3 = stuph[i];
-			} else if (i == 7) {
-				y3 = stuph[i];
-			} else if (i == 8) {
-				z3 = stuph[i];
-			} else if (i == 9) {
-				end = (int) stuph[i];
-			} else if (i > 9) {
-				break;
-			}
-		}
-	}
+	static double[][] triangles = null;
 	
 	@SuppressWarnings("static-access")
 	public String Init(String xml) throws IOException {
@@ -61,6 +34,8 @@ public class Convert {
 		System.out.println((TriCount*3)+" vertecies in XML");
 		System.out.println("Getting triangle data...");
 		
+		OBJExporter export = new OBJExporter();
+		
 		obj = obj + "#Brought to you by XanthicDragon\n\ng Model\n\n";
 		
 		Index index = new Index();
@@ -68,47 +43,11 @@ public class Convert {
 		int finish = string.find(xml, "</indices>", 0);
 		String stxt = xml.substring(start, finish);
 		int[] indices = index.getIndices(stxt);
-		int highest = index.getHighestIndex(stxt);
-		int leng = indices.length;
-		int finds = 0;
-		for (int i = 0; i < leng*2; i++) {
-			if (finds >= leng || i >= leng) {
-				if (i > 0 && i % 3 == 0) {
-					int g = indices[i-leng+2]+highest;
-					int h = indices[i-leng+1]+highest;
-					int j = indices[i-leng+0]+highest;
-					String t3 = g+" "+h+" "+j;
-					objF = objF + "f "+t3+"\n";
-				}
-			} else {
-				if (i > 0 && i % 3 == 0) {
-					int g = indices[i-2];
-					int h = indices[i-1];
-					int j = indices[i-0];
-					String t3 = g+" "+h+" "+j;
-					objF = objF + "f "+t3+"\n";
-					
-				}
-			}
-			finds += 1;
-		}
-		int gets = 0;
-		for (int i = 0; i < leng; i++) {
-			if (gets >= TriCount) {
-				end = 0;
-				gets = 0;
-			}
-			double[] items = tri.getAll(xml, end);
-			IterateTriangleDataTable(items);
-			obj = obj + "v "+x1+" "+y1+" "+z1+"\n";
-			obj = obj + "v "+x2+" "+y2+" "+z2+"\n";
-			obj = obj + "v "+x3+" "+y3+" "+z3+"\n";
-			gets += 1;
+		for (int i = 0; i < TriCount; i++) {
+			triangles[i] = tri.getAll(xml, end);
 		}
 		
-		obj = obj + "\nusemtl none\n\n";
-		System.out.println("Vertecies acquired and mapped. Getting vertex offsets...");
-		obj = obj + objF;
+		obj = export.getInput(triangles, indices);
 		
 		System.out.println("Complete!");
 		return obj;
