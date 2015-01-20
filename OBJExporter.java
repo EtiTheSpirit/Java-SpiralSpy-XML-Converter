054e267e90dea42f32e778caea1045cbed55377c
Brent Duanne <subzeroxan@gmail.com>
2015-01-20 15:22:00 -0600

    Updated to fix sideways meshes

--- OBJExporter.java
+++ OBJExporter.java
@@ -2,8 +2,6 @@ package start;
 
 public class OBJExporter {
 	
-	static String obj = "";
-	
 	static double x1;
 	static double x2;
 	static double x3;
@@ -16,13 +14,13 @@ public class OBJExporter {
 	static double z2;
 	static double z3;
 	
-	public static String getInput(double[][] tris, int[] indices) {
+	public static String getInput(double[][] tris, int[] indices, int triLen) {
+		String obj = "#Brought to you by XanthicDragon\n\ng Model\n\n";
 		/*
 		 * TRIS:
 		 * {input} {x1, y1, z1, x2, y2, z2, x3, y3, z3, end}
 		 * Ignore end
 		 */
-		int triLen = tris.length;
 		int indLen = indices.length;
 		/*
 		 * IDEA:
@@ -33,23 +31,42 @@ public class OBJExporter {
 		 * If there is a triangle capable of being indexed, run it in that order. OTHERWISE, go the normal order.
 		 */
 		for (int i = 0; i < triLen; i++) {
-			if (tris[indices[i]] != null) {
+			if (i < indLen) {
 				double[] set = tris[indices[i]];
-				x1 = set[0];
-				y1 = set[1];
-				z1 = set[2];
-				
-				x2 = set[3];
-				y2 = set[4];
-				z2 = set[5];
-				
-				x3 = set[6];
-				y3 = set[7];
-				z3 = set[8];
-				
-				obj = obj + "v "+x1+" "+y1+" "+z1+"\n";
-				obj = obj + "v "+x2+" "+y2+" "+z2+"\n";
-				obj = obj + "v "+x3+" "+y3+" "+z3+"\n";
+				if (set != null) {
+					x1 = set[0];
+					y1 = set[1];
+					z1 = set[2];
+					
+					x2 = set[3];
+					y2 = set[4];
+					z2 = set[5];
+					
+					x3 = set[6];
+					y3 = set[7];
+					z3 = set[8];
+					
+					obj = obj + "v "+x1+" "+y1+" "+z1+"\n";
+					obj = obj + "v "+x2+" "+y2+" "+z2+"\n";
+					obj = obj + "v "+x3+" "+y3+" "+z3+"\n";
+				} else {
+					set = tris[i];
+					x1 = set[0];
+					y1 = set[1];
+					z1 = set[2];
+					
+					x2 = set[3];
+					y2 = set[4];
+					z2 = set[5];
+					
+					x3 = set[6];
+					y3 = set[7];
+					z3 = set[8];
+					
+					obj = obj + "v "+x1+" "+y1+" "+z1+"\n";
+					obj = obj + "v "+x2+" "+y2+" "+z2+"\n";
+					obj = obj + "v "+x3+" "+y3+" "+z3+"\n";
+				}
 			} else {
 				double[] set = tris[i];
 				x1 = set[0];
@@ -72,8 +89,8 @@ public class OBJExporter {
 		
 		obj = obj + "\nusemtl null\n\n";
 		
-		for (int i = 0; i < indLen; i++) {
-			if (i > 0 && (i % 3 == 0 || i == 2)) {
+		for (int i = 0; i < triLen*3; i++) {
+			if (i % 3 == 0 && i > 0) {
 				obj = obj + "f "+(i-2)+" "+(i-1)+" "+(i-0)+"\n";
 			}
 		}
