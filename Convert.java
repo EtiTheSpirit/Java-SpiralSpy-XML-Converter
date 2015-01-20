ac11102bac8669c19c8c73a756e1f8c271a70e94
Brent Duanne <subzeroxan@gmail.com>
2015-01-20 15:23:17 -0600

    TODO: Fix skipping first few triangles.

--- Convert.java
+++ Convert.java
@@ -25,31 +25,48 @@ public class Convert {
 	
 	static int end = 0;
 	
-	static double[][] triangles = null;
-	
 	@SuppressWarnings("static-access")
 	public String Init(String xml) throws IOException {
-		int TriCount = string.match(xml, "<triangle>");
-		System.out.println(TriCount+" faces in XML.");
-		System.out.println((TriCount*3)+" vertecies in XML");
-		System.out.println("Getting triangle data...");
-		
-		OBJExporter export = new OBJExporter();
-		
-		obj = obj + "#Brought to you by XanthicDragon\n\ng Model\n\n";
-		
-		Index index = new Index();
-		int start = string.find(xml, "<indices>", 0);
-		int finish = string.find(xml, "</indices>", 0);
-		String stxt = xml.substring(start, finish);
-		int[] indices = index.getIndices(stxt);
-		for (int i = 0; i < TriCount; i++) {
-			triangles[i] = tri.getAll(xml, end);
+		if (string.find(xml, "com.threerings.export.XMLImporter", 0) > -1) {
+			int TriCount = string.match(xml, "<triangle>");
+			System.out.println(TriCount+" faces in XML.");
+			System.out.println((TriCount*3)+" vertecies in XML");
+			System.out.println("Getting triangle data...");
+			
+			double[][] triangles = new double[TriCount][9];
+			
+			OBJExporter export = new OBJExporter();
+			
+			obj = obj + "#Brought to you by XanthicDragon\n\ng Model\n\n";
+			
+			Index index = new Index();
+			int start = string.find(xml, "<indices>", 0);
+			int finish = string.find(xml, "</indices>", 0);
+			String stxt = xml.substring(start, finish);
+			int[] indices = index.getIndices(stxt);
+			/*for (int i = 0; i < TriCount; i++) {
+				double[] items = tri.getAll(xml, end);
+				triangles[i] = items;
+				end = (int) items[9];
+			}*/
+			
+			int i = 0;
+			
+			String xml2 = xml.substring(string.find(xml, "<mode>TRIANGLES</mode>", 0));
+			while (string.find(xml2, "<triangle>", end) > -1) {
+				System.out.println(end);
+				double[] items = tri.getAll(xml2, end);
+				triangles[i] = items;
+				end = (int) items[9];
+				i++;
+			}
+			
+			obj = export.getInput(triangles, indices, TriCount);
+			
+			System.out.println("Complete!");
+			return obj;
+		} else {
+			return "Not a valid SpiralSpy XML.";
 		}
-		
-		obj = export.getInput(triangles, indices);
-		
-		System.out.println("Complete!");
-		return obj;
 	}
 }
